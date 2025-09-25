621825ea47bb09ddb43755db0779e4351af8980c
PaulNarvik <111660098+PaulNarvik@users.noreply.github.com>
2025-09-25 21:54:49 +0200

    Build + test

---
+++ tests/largeint/test.c
@@ -0,0 +1,9 @@
+// Copier le contenu de ce fichier dans main afin de l'utiliser
+#include "largeint.h"
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  __uint128_t i = 0;
+  return 0;
+}
